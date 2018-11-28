1579a5556dc2813d45e36ed7e25e145039dc654f
martels <martel.s@husky.neu.edu>
2018-11-28 11:09:55 -0500

    update

---
+++ maze.h
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <limits.h>
+#include "d_except.h"
+#include <list>
+#include <fstream>
+#include "d_matrix.h"
+#include "graph.h"
+
+using namespace std;
+
+struct direction
+{
+    int dir[4]; //left, right, up, down
+};
+
+class maze
+{
+   public:
+      maze(ifstream &fin);
+      void print(int,int,int,int);
+      bool isLegal(int i, int j);
+
+      void setMap(int i, int j, int n);
+      int getMap(int i, int j) const;
+      void mapMazeToGraph(graph &g);
+      void getDir();
+
+   private:
+      int rows; // number of rows in the maze
+      int cols; // number of columns in the maze
+      
+      matrix<direction> possibleDir;
+      matrix<bool> value;
+      matrix<int> map;      // Mapping from maze (i,j) values to node index values
+};
+
+void maze::setMap(int i, int j, int n)
+// Set mapping from maze cell (i,j) to graph node n. 
+{
+    map[i][j] = n;
+}
+
+int maze ::getMap(int i, int j) const
+// Return mapping of maze cell (i,j) in the graph.
+{
+    return map[i][j];
+}
+
+maze::maze(ifstream &fin)
+// Initializes a maze by reading values from fin.  Assumes that the
+// number of rows and columns indicated in the file are correct.
+{
+   fin >> rows;
+   fin >> cols;
+
+   char x;
+
+   value.resize(rows,cols);
+   for (int i = 0; i <= rows-1; i++)
+      for (int j = 0; j <= cols-1; j++)
+      {
+	 fin >> x;
+	 if (x == 'O')
+            value[i][j] = true;
+	 else
+	    value[i][j] = false;
+      }
+
+   map.resize(rows,cols);
+   possibleDir.resize(rows, cols);
+}
+
+void maze::getDir()
+{
+    for( int i = 0; i < cols; i++)
+        for(int k = 0; k < rows; k++)
+        {
+            if(value[i][k-1])
+                possibleDir[i][k].dir[0] = 1;
+            if(value[i][k+1])
+                possibleDir[i][k].dir[1] = 1;
+            if(value[i-1][k])
+                possibleDir[i][k].dir[2] = 1;
+            if(value[i+1][k])
+                possibleDir[i][k].dir[3] = 1;
+        }
+
+    return;
+}
+
+void maze::print(int goalI, int goalJ, int currI, int currJ)
+// Print out a maze, with the goal and current cells marked on the
+// board.
+{
+   cout << endl;
+
+   if (goalI < 0 || goalI > rows || goalJ < 0 || goalJ > cols)
+      throw rangeError("Bad value in maze::print");
+
+   if (currI < 0 || currI > rows || currJ < 0 || currJ > cols)
+      throw rangeError("Bad value in maze::print");
+
+   for (int i = 0; i <= rows-1; i++)
+   {
+      for (int j = 0; j <= cols-1; j++)
+      {
+	 if (i == goalI && j == goalJ)
+	    cout << "*";
+	 else
+	    if (i == currI && j == currJ)
+	       cout << "+";
+	    else
+	       if (value[i][j])
+		  cout << " ";
+	       else
+		  cout << "X";	  
+      }
+      cout << endl;
+   }
+   cout << endl;
+}
+
+bool maze::isLegal(int i, int j)
+// Return the value stored at the (i,j) entry in the maze.
+{
+   if (i < 0 || i > rows || j < 0 || j > cols)
+      throw rangeError("Bad value in maze::isLegal");
+
+   return value[i][j];
+}
+
+void maze::mapMazeToGraph(graph &g)
+// Create a graph g that represents the legal moves in the maze m.
+{
+    int i = 0, j = 0;
+    while(i != rows-1 && j != cols-1)
+    {
+        
+    }
+}
+
